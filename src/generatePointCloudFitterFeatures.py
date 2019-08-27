72095cde70342e09d2664f93670e5e4ccec3f78a
tonnesfn <tonnes.nygaard@gmail.com>
2019-08-27 14:52:43 +1000

    Added a node to generate features for all bag files

---
+++ src/generatePointCloudFitterFeatures.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import roslib
+roslib.load_manifest('terrain_characterizer')
+import sys
+import rospy
+import subprocess
+import ntpath
+import progressbar
+from terrain_characterizer.srv import *
+
+import os
+import os.path
+
+def startLogging(givenServiceProxy, givenLogPath):
+    givenServiceProxy(givenLogPath)
+
+def stopLogging(givenServiceProxy):
+    givenServiceProxy("")
+
+# Find all bag files
+bagFiles = []
+
+for dirpath, dirnames, filenames in os.walk("/home/nyg003/catkin_ws/experimentResults/"):
+    for filename in [f for f in filenames if f.endswith(".bag")]:
+        bagFiles.append(os.path.join(dirpath, filename))
+
+bagFiles.sort()
+
+for bagFile in progressbar.progressbar(bagFiles):
+    bagName = ntpath.basename(bagFile)
+    bagDirectory = ntpath.dirname(bagFile)
+
+    # Do service call to setup logging
+    rospy.wait_for_service('/dyret/pointCloudPlaneFitter/featureLogging')
+
+    try:
+        featureLogging = rospy.ServiceProxy('/dyret/pointCloudPlaneFitter/featureLogging', featureLoggingService)
+    except rospy.ServiceException, e:
+        print("Service call failed: %s"%e)
+
+    startLogging(featureLogging, bagFile.replace(".bag", "_features.csv"))
+
+    # Play bag file
+    FNULL = open(os.devnull, 'w')
+    process = subprocess.Popen(['rosbag', 'play', bagName], cwd=bagDirectory, stdout=FNULL)
+    process.wait()
+
+    # Stop logging
+    stopLogging(featureLogging)
+
